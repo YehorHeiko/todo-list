0508d3b69f70375204a0b7d9db03f3f974997693
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-11-28 23:10:15 +0200

    Make Project

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,16 @@
+
+import List from "./components/List";
+import "./index.css"
+
+function App() {
+
+
+
+  return (
+    <div className="main">
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do-list/src/components/List.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import styles from "./List.module.css";
+interface Todo {
+  id: number;
+  text1: string;
+  text2: string;
+}
+
+function List() {
+  const [todo, setTodo] = useState<Todo[]>([]);
+  const [inputOne, setInputOne] = useState("");
+  const [inputTwo, setInputTwo] = useState("");
+
+  function addProperties() {
+    setTodo((e) => [
+      ...e,
+      { id: Date.now(), text1: inputOne, text2: inputTwo },
+    ]);
+  }
+  function removeProperties() {
+    setTodo((e) => e.id !== id ? );
+  }
+
+  return (
+    <div className={styles.bg}>
+      <div className={styles.block}>
+        <h1>ToDoList</h1>
+        <div className={styles.to_do_block}>
+          <input
+            onChange={(e) => setInputOne(e.target.value)}
+            type="text"
+            value={inputOne}
+            className={styles.input}
+          />
+          <input
+            onChange={(e) => setInputTwo(e.target.value)}
+            type="text"
+            value={inputTwo}
+            className={styles.input}
+          />
+          <button onClick={addProperties}>ADD ToDo</button>
+
+          <ul>
+            {todo.map((e) => (
+              <p key={e.id}>
+                {e.text1} {e.text2}
+                <button>DEL</button>
+              </p>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default List;
